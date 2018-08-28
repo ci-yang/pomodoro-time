81385408bae5945573bbb94fe24dfbf2451a794d
ciyang <jason21131@gmail.com>
2018-08-28 13:54:02 +0800

    Set the appearance and build some assets.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const {app, BrowserWindow} = require('electron');
+const pomodoro = require('./pomodoro');
+const path = require('path');
 
 app.on('ready', _ => {
     win = new BrowserWindow({
         width: 400,
         height: 400,
     });
+
+    pomodoro();
+
+    win.loadURL(`file://${__dirname}/pomodoro.html`);
 
     win.on('closed', _ =>{
         win = null;
